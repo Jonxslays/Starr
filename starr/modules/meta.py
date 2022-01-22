c8dcea764279c6dabf036747cbce7a1a954e584c
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2022-01-22 15:45:48 -0700

    Combine slash and message commands

--- starr/modules/meta.py
+++ starr/modules/meta.py
@@ -40,7 +40,11 @@ from starr.bot import StarrBot
 meta = tanjun.Component(name="meta").add_check(tanjun.checks.GuildCheck())
 
 
-async def _ping(ctx: tanjun.abc.Context, bot: StarrBot) -> None:
+@meta.with_command
+@tanjun.as_slash_command("ping", "Starr's latency.")
+@meta.with_command
+@tanjun.as_message_command("ping", "Starr's latency.")
+async def ping(ctx: tanjun.abc.Context, bot: StarrBot = tanjun.inject(type=StarrBot)) -> None:
     start = time.perf_counter()
     message = await ctx.respond(".", ensure_result=True)
     elapsed = time.perf_counter() - start
@@ -48,24 +52,6 @@ async def _ping(ctx: tanjun.abc.Context, bot: StarrBot) -> None:
     await message.edit(
         f"Gateway: {bot.heartbeat_latency * 1000:,.2f} ms\nRest: {elapsed * 1000:,.2f} ms"
     )
-
-
-@meta.with_command
-@tanjun.as_slash_command("ping", "Starr's latency.")
-async def ping_slash_command(
-    ctx: tanjun.abc.Context,
-    bot: StarrBot = tanjun.inject(type=StarrBot),
-) -> None:
-    await _ping(ctx, bot)
-
-
-@meta.with_command
-@tanjun.as_message_command("ping", "Starr's latency.")
-async def ping_message_command(
-    ctx: tanjun.abc.Context,
-    bot: StarrBot = tanjun.inject(type=StarrBot),
-) -> None:
-    await _ping(ctx, bot)
 
 
 @tanjun.as_loader
