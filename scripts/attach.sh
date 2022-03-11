1359c2d53353a373fffdb799a419a070f719b4a0
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2022-03-11 08:10:01 -0700

    Update scripts

--- scripts/attach.sh
+++ scripts/attach.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This script is used to attach to the running docker container via shell.
-CONTAINER=$(docker ps | grep starr_starr-app | awk '{print $1}')
+CONTAINER=$(docker ps | grep $1 | awk '{print $1}')
 
 echo "Attaching to $CONTAINER"
 docker container exec -it $CONTAINER /bin/bash
--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,4 +2,4 @@
 
 # This script is used to restart the app.
 # $1 is used to add --build if necessary.
-./scripts/kill.sh && ./scripts/start.sh $1
+./scripts/kill.sh && ./scripts/start.sh $1 $2
